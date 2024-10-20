c7eb250179211580953dadee65b140687f15bcff
Sergey Mikheev <serega9337@gmail.com>
2024-10-20 17:12:57 +0500

    dded ProductCategory Model

--- core/models/product.py
+++ core/models/product.py
@@ -1,4 +1,9 @@
-from sqlalchemy.orm import Mapped
+from typing import List
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.dialects.mssql.information_schema import constraints
+from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.testing.schema import mapped_column
 
 from .base import Base
 
@@ -6,3 +11,14 @@ class Product(Base):
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
+    category_id: Mapped[int]=mapped_column(
+        ForeignKey("productcategorys.id"),
+    )
+
+    category: Mapped["ProductCategory"] = relationship(back_populates="product")
+
+
+class ProductCategory(Base):
+    category: Mapped[str]
+
+    product: Mapped[List["Product"]] = relationship(back_populates="category")
